08d95c7abaaab47a786bf1f679b7e04e6872a5da
Devan <devanasokan@gmail.com>
2026-08-20 00:03:51 +0800

    try again dawg

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "--- 1. Installing temporary CPU-only PyTorch and onnxscript first ---"
-pip install torch onnxscript --index-url https://download.pytorch.org/whl/cpu
+echo "--- 1. Installing temporary CPU-only PyTorch from PyTorch index ---"
+pip install torch --index-url https://download.pytorch.org/whl/cpu
 
-echo "--- 2. Installing remaining production requirements ---"
+echo "--- 2. Installing onnxscript and remaining production requirements from PyPI ---"
+pip install onnxscript
 pip install -r requirements.txt
 
 echo "--- 3. Running export script to generate onnx_model/ on the server ---"
